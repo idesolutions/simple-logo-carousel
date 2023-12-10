5a37c2846e313001d4e298b43912fbfd33ac8692
sunjava11 <sunjava11@gmail.com>
2023-12-10 18:36:04 +0500

    plugin integration tests

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: tvledesign, ideinteractive
 Tags: logo carousel, logo slider, carousel, slider
 Requires at least: 5.0
-Tested up to: 6.0.2
+Tested up to: 6.4.2
 Requires PHP: 7.4
 Stable tag: 1.9.1
 License: GPLv2 or later
---
+++ tests/test-sample.php
@@ -0,0 +1,22 @@
+<?php
+
+use PLSimpleLogoCarousel\Base\BaseController;
+
+/**
+ * Class SampleTest
+ *
+ * @package Simple_Logo_Carousel
+ */
+
+
+
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_plugin_path() {		
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
